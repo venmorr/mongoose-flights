823a13b9b0e1e967f4dca52fe1f51de9fd9bee7d
venmorr <stevenmorrrca@email.com>
2023-09-16 11:09:07 -0400

    connect mongoose

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import createError from 'http-errors'
 import logger from 'morgan'
+import 'config/database.js'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
